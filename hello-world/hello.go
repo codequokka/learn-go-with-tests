a0cbb543ac2505f3e0882d02dfdbf1ba4db2ce8e
Nobuchika Tanaka <codequokka@gmail.com>
2022-10-31 07:04:38 +0900

    Add greetingPrefix()

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -14,16 +14,23 @@ func Hello(name, language string) string {
 		name = "world"
 	}
 
-	prefix := englishHelloPrefix
+	prefix := greetingPrefix(language)
 
+	return prefix + name
+}
+
+// Hello returns a greeting prefix in a given language.
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return
 }
 
 func main() {
