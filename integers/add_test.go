b80de06f9612a23f99b376d67dc0ababaca38ba2
Nobuchika Tanaka <codequokka@gmail.com>
2022-11-03 07:16:35 +0900

    Add Add()

---
+++ integers/add_test.go
@@ -0,0 +1,14 @@
+package integers
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	got := Add(1, 2)
+	want := 3
+
+	if got != want {
+		t.Errorf("got: '%d', want: '%d'", got, want)
+	}
+}
